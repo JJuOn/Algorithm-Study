f26737ea6e793ad38aed04b2ac50592ccf24213d
서주원 <jwseo001@khu.ac.kr>
2020-06-12 10:57:07 +0900

    Fixed 7576 Runtime error

--- 7000~7999/7576.cpp
+++ 7000~7999/7576.cpp
@@ -7,36 +7,6 @@ using namespace std;
 int tomato[1000][1000];
 int day[1000][1000];
 
-bool canRipe(int n, int m) {
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++) {
-			if (tomato[i][j] == 0) {
-				bool flag = false;
-				if (i - 1 >= 0 && tomato[i - 1][j] != -1) {
-					flag = true;
-					continue;
-				}
-				if (i + 1 < n && tomato[i + 1][j] != -1) {
-					flag = true;
-					continue;
-				}
-				if (j - 1 >= 0 && tomato[i][j - 1] != -1) {
-					flag = true;
-					continue;
-				}
-				if (j + 1 < m && tomato[i][j + 1] != -1) {
-					flag = true;
-					continue;
-				}
-				if (!flag) {
-					return false;
-				}
-			}
-		}
-	}
-	return true;
-}
-
 int BFS(int green, int n, int m) {
 	queue<pair<int, int>> q;
 	for (int i = 0; i < n; i++) {
@@ -47,44 +17,57 @@ int BFS(int green, int n, int m) {
 			}
 		}
 	}
+	int result = 0;
 	while (green) {
+		if (green!=0 && q.empty()){
+			return 0;
+		}
 		pair<int, int> p = q.front();
 		q.pop();
+		//up
 		if (p.first - 1 >= 0 && tomato[p.first - 1][p.second] == 0) {
 			q.push(make_pair(p.first - 1, p.second));
 			tomato[p.first - 1][p.second] = 1;
 			day[p.first - 1][p.second] = day[p.first][p.second] + 1;
 			if (--green == 0) {
-				return day[p.first - 1][p.second];
+				result = day[p.first - 1][p.second];
+				break;
 			}
 		}
+		//down
 		if (p.first + 1 < n && tomato[p.first + 1][p.second] == 0) {
 			q.push(make_pair(p.first + 1, p.second));
 			tomato[p.first + 1][p.second] = 1;
 			day[p.first + 1][p.second] = day[p.first][p.second] + 1;
 			if (--green == 0) {
-				return day[p.first + 1][p.second];
+				result = day[p.first + 1][p.second];
+				break;
 			}
 		}
+		//left
 		if (p.second - 1 >= 0 && tomato[p.first][p.second - 1] == 0) {
 			q.push(make_pair(p.first, p.second - 1));
 			tomato[p.first][p.second - 1] = 1;
 			day[p.first][p.second - 1] = day[p.first][p.second] + 1;
 			if (--green == 0) {
-				return day[p.first][p.second - 1];
+				result = day[p.first][p.second - 1];
+				break;
 			}
 		}
+		//right
 		if (p.second + 1 < m && tomato[p.first][p.second + 1] == 0) {
 			q.push(make_pair(p.first, p.second + 1));
 			tomato[p.first][p.second + 1] = 1;
 			day[p.first][p.second + 1] = day[p.first][p.second] + 1;
 			if (--green == 0) {
-				return day[p.first][p.second + 1];
+				result = day[p.first][p.second + 1];
+				break;
 			}
 		}
 	}
+	return result;
 }
-		
+
 
 int main() {
 	ios::sync_with_stdio(false);
@@ -108,9 +91,6 @@ int main() {
 	}
 	if (empty + ripe == n * m) {
 		cout << "0\n";
-	}
-	else if (!canRipe(n, m)) {
-		cout << "-1\n";
 	}
 	else {
 		cout << BFS(green, n, m) - 1 << "\n";
