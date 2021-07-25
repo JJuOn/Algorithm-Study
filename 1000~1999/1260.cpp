22a20bb7667ab21c33661531c456188907ed7087
JJuOn <jwseo001@khu.ac.kr>
2021-07-25 16:13:00 +0900

    Update 1260(DFS,BFS), 9465(DP)

--- 1000~1999/1260.cpp
+++ 1000~1999/1260.cpp
@@ -1,39 +1,37 @@
 #include <iostream>
 #include <algorithm>
-#include <stack>
 #include <queue>
 #include <vector>
 
 using namespace std;
 
-void DFS(vector<int> a[], int currentNode, stack<int> s, int n, bool& found, vector<bool>& visited) {
-	s.push(currentNode);
-	visited[currentNode] = true;
-	cout << currentNode << " ";
-	if (a[currentNode].size() == 0) {
-		return;
-	}
-	for (int i = 0; i < a[currentNode].size(); i++) {
-		if (!visited[a[currentNode][i]]) {
-			DFS(a, a[currentNode][i], s, n, found, visited);
-		}
-		if (found) {
-			return;
+vector<vector<int>> a;
+vector<bool> visited;
+
+void DFS(int node) {
+	cout << node << " ";
+	visited[node] = true;
+	int size = a[node].size();
+	for (int i = 0; i < size; i++) {
+		if (!visited[a[node][i]]) {
+			DFS(a[node][i]);
 		}
 	}
 }
 
-void BFS(vector<int> a[], int currentNode, queue<int> q, int n, bool& found, vector<bool>& visited) {
-	q.push(currentNode);
-	visited[currentNode] = true;
-	while (!q.empty() && !found) {
+void BFS(int startNode) {
+	queue<int> q;
+	q.push(startNode);
+	visited[startNode] = true;
+	while (!q.empty()) {
 		int node = q.front();
 		q.pop();
 		cout << node << " ";
-		for (int i = 0; i < a[node].size(); i++) {
+		int size = a[node].size();
+		for (int i = 0; i < size; i++) {
 			if (!visited[a[node][i]]) {
-				visited[a[node][i]] = true;
 				q.push(a[node][i]);
+				visited[a[node][i]] = true;
 			}
 		}
 	}
@@ -44,30 +42,22 @@ int main() {
 	cout.tie(NULL);
 	cin.tie(NULL);
 	int n, m, v;
-	vector<int>* a, route;
-	stack<int> s;
-	queue<int> q;
-	bool found = false;
 	cin >> n >> m >> v;
-	vector<bool> visited(n + 1, false);
-	a = new vector<int>[n + 1];
+	a.resize(n + 1);
+	visited.resize(n + 1, false);
 	for (int i = 0; i < m; i++) {
 		int b, c;
 		cin >> b >> c;
 		a[b].push_back(c);
 		a[c].push_back(b);
 	}
-	for (int i = 0; i < n; i++) {
+	for (int i = 0; i <= n; i++) {
 		sort(a[i].begin(), a[i].end());
 	}
-	DFS(a, v, s, n, found, visited);
+	DFS(v);
 	cout << "\n";
-	route.clear();
-	for (int i = 0; i < visited.size(); i++) {
-		visited[i] = false;
-	}
-	found = false;
-	BFS(a, v, q, n, found, visited);
-	delete[] a;
+	fill(visited.begin(), visited.end(), false);
+	BFS(v);
+	cout << "\n";
 	return 0;
 }
--- 9000~9999/9465.cpp
+++ 9000~9999/9465.cpp
@@ -24,12 +24,24 @@ int main() {
 		}
 		int maxValue = -1;
 		dp[1][1] = a[1][1];
+		if (dp[1][1] > maxValue) {
+			maxValue = dp[1][1];
+		}
 		dp[2][1] = a[2][1];
+		if (dp[2][1] > maxValue) {
+			maxValue = dp[2][1];
+		}
 		dp[1][2] = dp[2][1] + a[1][2];
+		if (dp[1][2] > maxValue) {
+			maxValue = dp[1][2];
+		}
 		dp[2][2] = dp[1][1] + a[2][2];
+		if (dp[2][2] > maxValue) {
+			maxValue = dp[2][2];
+		}
 		for (int j = 3; j <= n; j++) {
-			dp[1][j] = max(max(dp[1][j - 2] + a[1][j], dp[2][j - 2] + a[1][j]), dp[2][j - 1] + a[1][j]);
-			dp[2][j] = max(max(dp[1][j - 2] + a[2][j], dp[2][j - 2] + a[2][j]), dp[1][j - 1] + a[2][j]);
+			dp[1][j] = max(max(dp[1][j - 2], dp[2][j - 2]), dp[2][j - 1]) + a[1][j];
+			dp[2][j] = max(max(dp[1][j - 2], dp[2][j - 2]), dp[1][j - 1]) + a[2][j];
 			if (dp[1][j] > maxValue) {
 				maxValue = dp[1][j];
 			}
